0836091c8ad9543cc17bdffa41d46e9308a8aced
Shash <ashishsinghlogic@gmail.com>
2025-07-18 19:42:27 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Bowlby_One_SC, DM_Mono } from "next/font/google";
 import "./globals.css";
+import { Header } from "@/components/Header";
 
 
 const bowlby = Bowlby_One_SC({
@@ -32,6 +33,7 @@ export default function RootLayout({
       <body className={`${bowlby.variable} ${dmMono.variable} antialiased font-mono font-medium text-zinc-800`}>
 
         <main>
+          <Header/>
         {children}
         </main>
 
