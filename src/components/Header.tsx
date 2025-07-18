c94d1ced3430041d51a9ce7a88a8e359e44cae8f
Shash <ashishsinghlogic@gmail.com>
2025-07-18 20:08:41 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,34 @@
+import Link from 'next/link'
 import React from 'react'
+import { ButtonLink } from './ButtonLink'
 
 type Props = {}
 
-export function Header({}: Props) {
-  return (
-    <header> header </header>
-  )
+export function Header({ }: Props) {
+    return (
+        <header>
+            <div className="flex">
+                <Link href="/">Logo</Link>
+
+
+                <nav>
+                    <ul>
+                        <li>
+                            boards
+                        </li>
+                    </ul>
+                </nav>
+
+                <div className="">
+                    <ButtonLink href = "" color='purple' 
+                    aria-label = "Cart(1)">
+                        Cart (1)
+                    </ButtonLink>
+                </div>
+            </div>
+
+
+
+        </header>
+    )
 }
