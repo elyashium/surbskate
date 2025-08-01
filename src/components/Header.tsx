b7cfae5f24909899ac49c5e0ac9c137b4cd59f77
Shash <ashishsinghlogic@gmail.com>
2025-08-01 15:02:11 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,18 +10,22 @@ export function Header({ }: Props) {
             <div className="mx-auto grid w-full max-w-6xl grid-cols-[auto,auto] items-center gap-6 md:grid-cols-[1fr,auto,1fr]">
                 <Link href="/" className='justify-self-start'>Logo</Link>
                 <nav aria-label='Main' className='col-span-full row-start-2 md:col-span-1 md:col-start-2 md:row-start-1'>
-                    <ul>
-                        <li>
-                            boards
-                        </li>
+                    <ul className='flex flex-wrap gap-8 items-centre justify-center'>
+                        <li>boards</li>
+                        <li>boards</li>
+                        <li>boards</li>
+                        <li>boards</li>
+                        <li>boards</li>
                     </ul>
                 </nav>
 
-                <div className="">
+                <div className="justify-self-end">
                     <ButtonLink href = "" color='purple' 
                     aria-label = "Cart(1)">
                         Cart (1)
                     </ButtonLink>
+                    <span className='hidden md:inline'>cart(1)</span>
+                    <span className='md:hidden'>1</span>
                 </div>
             </div>
 
