79aa949ee415fd5e6d5f57cf7ec6efa824ce099c
Shash <ashishsinghlogic@gmail.com>
2025-08-01 14:52:55 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ type Props = {}
 export function Header({ }: Props) {
     return (
         <header className='header absolute top-0 left-0 right-0 z-50 ~h-32/48 ~px-4/6 ~py-4/6 hd:h-32'>
-            <div className="flex">
+            <div className="mx-auto grid w-full max-w-6xl grid-cols-[auto,auto] items-center gap-6 md:grid-cols-[1fr,auto,1fr]">
                 <Link href="/">Logo</Link>
 
 
