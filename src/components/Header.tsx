6da75f2b8bfd09bf6ec7c85748c57a3749dd8694
Shash <ashishsinghlogic@gmail.com>
2025-08-01 14:57:18 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,10 +8,8 @@ export function Header({ }: Props) {
     return (
         <header className='header absolute top-0 left-0 right-0 z-50 ~h-32/48 ~px-4/6 ~py-4/6 hd:h-32'>
             <div className="mx-auto grid w-full max-w-6xl grid-cols-[auto,auto] items-center gap-6 md:grid-cols-[1fr,auto,1fr]">
-                <Link href="/">Logo</Link>
-
-
-                <nav>
+                <Link href="/" className='justify-self-start'>Logo</Link>
+                <nav aria-label='Main' className='col-span-full row-start-2 md:col-span-1 md:col-start-2 md:row-start-1'>
                     <ul>
                         <li>
                             boards
